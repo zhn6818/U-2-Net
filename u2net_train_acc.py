86f11bb1d77d2be749300ce88e79722f48d429dc
张海宁 <zhn6818@163.com>
2025-03-13 12:41:03 +0800

    update loss0

--- u2net_train_acc.py
+++ u2net_train_acc.py
@@ -38,8 +38,11 @@ def muti_bce_loss_fusion(d0, d1, d2, d3, d4, d5, d6, labels_v):
 	loss5 = bce_loss(d5,labels_v)
 	loss6 = bce_loss(d6,labels_v)
 
-	loss = loss0 + loss1 + loss2 + loss3 + loss4 + loss5 + loss6
-	print("l0: %3f, l1: %3f, l2: %3f, l3: %3f, l4: %3f, l5: %3f, l6: %3f\n"%(loss0.data.item(),loss1.data.item(),loss2.data.item(),loss3.data.item(),loss4.data.item(),loss5.data.item(),loss6.data.item()))
+	# 增加d0的权重
+	loss = 2.0 * loss0 + 0.5 * (loss1 + loss2 + loss3 + loss4 + loss5 + loss6)
+	print("l0: %3f, l1: %3f, l2: %3f, l3: %3f, l4: %3f, l5: %3f, l6: %3f\n"%(
+		loss0.data.item(),loss1.data.item(),loss2.data.item(),loss3.data.item(),
+		loss4.data.item(),loss5.data.item(),loss6.data.item()))
 
 	return loss0, loss
 
